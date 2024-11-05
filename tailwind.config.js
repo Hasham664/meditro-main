d41f621d1d36fb9adb24a17e996df0c3e2498fa7
Hasham664 <m.hasham3222@gmail.com>
2024-11-05 19:39:03 +0500

    comit

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,21 +16,6 @@ module.exports = {
       screens: {
         tablet: "840px",
       },
-      animation: {
-        marquee: "marquee 28s linear infinite",
-      },
-
-      keyframes: {
-        marquee: {
-          "0%": { transform: "translateX(100%)" },
-          "100%": { transform: "translateX(-200%)" },
-        },
-      },
-      variants: {
-        extend: {
-          animation: ["hover", "focus"],
-        },
-      },
     },
   },
   plugins: [],
